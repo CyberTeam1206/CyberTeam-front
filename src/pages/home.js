5b727bc5e8cc4677e97bd0793d212ff56b5ede45
Тарас <taras06031@gmail.com>
2020-05-11 23:19:23 +0300

    frontend works

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,29 +2,29 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
-import Scream from '../components/Scream';
-export class home extends Component {
-    state = {
-        screams: null
+//import Scream from '../components/Scream';
+class home extends Component {
+    state ={
+        screams:null
     };
     componentDidMount() {
-axios.get('../Handlers/screams')
+axios.get("http://localhost:5000/cyberrr-8219c/europe-west1/api/screams")
     .then(res =>{
-        console.log(res.data)
+        console.log(res.data);
 this.setState({
     screams:res.data
 })
     })
-    .catch(err=> console.log(err));
+    .catch(err => console.log(err));
     }
 
     render(){
         let recentScreamsMarkup = this.state.screams ?(
-            this.state.screams.map(scream => <Scream scream={scream}/>)
-        ) :(
+            this.state.screams.map((scream) => <p>{scream.body}</p>)
+        ) : (
             <p>Loading...</p>);
         return (
-          <Grid container spacing={8}>
+          <Grid container spacing={10}>
               <Grid item sm={8} xs={12}>
                   {recentScreamsMarkup}
               </Grid>
